90ee28b32fcb9ae0c41d6b03c2f979b8a06bb950
Tony Cardenas <antoniocal@gmail.com>
2017-12-13 15:11:21 -0600

    Limpieza de codigo.

--- fancontrol.c
+++ fancontrol.c
@@ -2,54 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main (int argc, char* argv[])
 {
-        long temp;
-        if(argc < 2){
-		wiringPiSetup () ;
-                pinMode (7, OUTPUT) ;
-                for (;;)
-                {
-                        digitalWrite (7, HIGH) ; delay (10) ;
-                        digitalWrite (7,  LOW) ; delay (40) ;
-                }
+	long temp;
+	wiringPiSetup ();
+	if(argc < 2){
+		pinMode (7, OUTPUT);
+		for (;;)
+		{
+			digitalWrite (7, HIGH);
+			delay(10);
+			digitalWrite (7,  LOW);
+			delay(40);
+		}
 	}
-
-        temp = strtol(argv[1], NULL, 10);
-
-        if(temp <= 32){
-                wiringPiSetup () ;
-                pinMode (7, OUTPUT) ;
-                for (;;)
-                {
-                        digitalWrite (7, HIGH) ; delay (10) ;
-                        digitalWrite (7,  LOW) ; delay (40) ;
-                }
-        } else if (temp <= 45 && temp > 32) {
-                wiringPiSetup () ;
-                pinMode (7, OUTPUT) ;
-                for (;;)
-                {
-                        digitalWrite (7, HIGH) ; delay (10) ;
-                        digitalWrite (7,  LOW) ; delay (10) ;
-                }
-        } else if (temp > 45 && temp < 60) {
-                wiringPiSetup () ;
-                pinMode (7, OUTPUT) ;
-                for (;;)
-                {
-                        digitalWrite (7, HIGH) ; delay (40) ;
-                        digitalWrite (7,  LOW) ; delay (10) ;
-                }
-        } else {
-		wiringPiSetup () ;
-                pinMode (7, OUTPUT) ;
-                for (;;)
-                {
-                        digitalWrite (7, HIGH) ; delay (1000) ;
-                }
+	temp = strtol(argv[1], NULL, 10);
+	if(temp <= 32){
+		pinMode (7, OUTPUT) ;
+		for (;;)
+		{
+			digitalWrite (7, HIGH) ; delay (10);
+			digitalWrite (7,  LOW) ; delay (40);
+		}
+	} else if (temp <= 45 && temp > 32)
+	{
+		pinMode (7, OUTPUT) ;
+		for (;;)
+		{
+			digitalWrite (7, HIGH);
+			delay(10);
+			digitalWrite (7,  LOW);
+			delay(10);
+		}
+	} else if (temp > 45 && temp < 60)
+	{
+		pinMode (7, OUTPUT) ;
+		for (;;)
+		{
+			digitalWrite (7, HIGH);
+			delay (40);
+			digitalWrite (7,  LOW);
+			delay(10);
+		}
+	} else
+	{
+		pinMode (7, OUTPUT);
+		for (;;)
+		{
+			digitalWrite (7, HIGH);
+			delay(1000);
+		}
 	}
-
-
-        return 0 ;
+	return 0;
 }
